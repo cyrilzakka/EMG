164f25a31ac5d856b4238b6aecf6dbcbe9742352
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2024-11-20 10:18:26 -0800

    Initial Commit

---
+++ EMG/EMGApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMGApp.swift
+//  EMG
+//
+//  Created by Cyril Zakka on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct EMGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
