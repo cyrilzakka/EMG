ed78ccdea4201f5ffa2df9a6d58db634b659a43d
Cyril Zakka <1841186+cyrilzakka@users.noreply.github.com>
2024-11-21 19:44:21 -0800

    Fixed spacing to prevent jitter

--- EMG/AppDelegate.swift
+++ EMG/AppDelegate.swift
@@ -12,13 +12,13 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     @State private var powerMonitor = PowerMonitor()
-    let statusItem = NSStatusBar.system.statusItem(withLength: 250)
+    let statusItem = NSStatusBar.system.statusItem(withLength: 275)
     
     func applicationDidFinishLaunching(_ notification: Notification) {
         let menuBarView = MenuView()
             .environment(powerMonitor)
         let hostingView = NSHostingView(rootView: menuBarView)
-        hostingView.frame.size = NSSize(width: 250, height: 25)
+        hostingView.frame.size = NSSize(width: 275, height: 25)
 //        hostingView.translatesAutoresizingMaskIntoConstraints = false
         
         if let button = statusItem.button {
--- EMG/Views/MenuView.swift
+++ EMG/Views/MenuView.swift
@@ -12,8 +12,10 @@ struct MenuView: View {
     @Environment(PowerMonitor.self) private var powerMonitor
     
     var body: some View {
+        // cpu_pwr: EMG.chartInfo(title: "CPU: 1.51W", val: [1.1142857, 1.8857144, 1.8000001, 7.5428567]),
+//        gpu_pwr: EMG.chartInfo(title: "GPU: 0.17W", val: [0.0, 0.114285715, 0.25714287, 0.82857144]),
         ZStack {
-            HStack(alignment: .center, spacing: 4) {
+            HStack(alignment: .center, spacing: 0) {
                 // GPU
                 HStack(alignment: .center, spacing: 5) {
                     VerticalGauge(value: Int(powerMonitor.globalInfo?.pcpu_usg.val ?? 0), minRange: 0, maxRange: 100).drawingGroup()
@@ -27,24 +29,28 @@ struct MenuView: View {
                     .fontDesign(.rounded)
                     .scaleEffect(0.8)
                 }
+                .frame(width: 70)
                 
                 // CPU
-                HStack(alignment: .center, spacing: 2) {
-                    VerticalGauge(value: Int(powerMonitor.globalInfo?.ecpu_usg.val ?? 0), minRange: 0, maxRange: 100)
-                    VerticalGauge(value: Int(powerMonitor.globalInfo?.pcpu_usg.val ?? 0), minRange: 0, maxRange: 100)
+                HStack(alignment: .center, spacing: 0) {
+                    HStack(alignment: .center, spacing: 2) {
+                        VerticalGauge(value: Int(powerMonitor.globalInfo?.ecpu_usg.val ?? 0), minRange: 0, maxRange: 100)
+                        VerticalGauge(value: Int(powerMonitor.globalInfo?.pcpu_usg.val ?? 0), minRange: 0, maxRange: 100)
+                    }
+                    .drawingGroup()
+                    
+                    VStack(alignment: .leading) {
+                        Text("E-CPU: \(Int(powerMonitor.globalInfo?.ecpu_usg.val ?? 0), format: .number)%")
+                        Text("P-CPU: \(Int(powerMonitor.globalInfo?.pcpu_usg.val ?? 0), format: .number)%")
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                    .fontDesign(.rounded)
+                    .scaleEffect(0.8)
+                    
                 }
-                .drawingGroup()
-                .frame(maxHeight: .infinity)
-                
-                VStack(alignment: .leading) {
-                    Text("E-CPU: \(Int(powerMonitor.globalInfo?.ecpu_usg.val ?? 0), format: .number)%")
-                    Text("P-CPU: \(Int(powerMonitor.globalInfo?.pcpu_usg.val ?? 0), format: .number)%")
-                }
-                .font(.footnote)
-                .fontWeight(.semibold)
-                .fontDesign(.rounded)
-                .scaleEffect(0.8)
-                .layoutPriority(100)
+                .frame(width: 80)
                 
                 // ANE
                 VStack(alignment: .leading) {
@@ -54,65 +60,54 @@ struct MenuView: View {
                         Image(systemName: "cpu.fill")
                             .foregroundStyle(.primary.opacity(0.8))
                     }
-                    
                     .labelStyle(NoSpacingLabel(spacing: 3))
                     
                     Text("\(Int(powerMonitor.globalInfo?.ane_usg.val ?? 0), format: .number)%")
-                    
                 }
-                .scaleEffect(0.8)
-                
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .fontDesign(.rounded)
-                
+                .scaleEffect(0.8)
+//                .frame(width: 75)
                 
                 // Read/Write
-                let (total, read, write) = parseMemoryBandwidth(powerMonitor.globalInfo?.bw_grp ?? "")
+                let (_, read, write) = parseMemoryBandwidth(powerMonitor.globalInfo?.bw_grp ?? "")
                 VStack(alignment: .leading, spacing: 0) {
                     Label {
                         Text("R: \(Int(read), format: .number) GB/s")
+                            .font(.footnote)
+                            .fontDesign(.rounded)
+                            .fontWeight(.semibold)
+                            .fixedSize()
                     } icon: {
                         Image(systemName: "arrow.up")
                             .foregroundStyle(.primary.opacity(0.8))
-                            .fontWeight(.black)
+                            .fontWeight(.bold)
+                            .imageScale(.small)
                     }
                     .labelStyle(NoSpacingLabel(spacing: 3))
                     
                     Label {
                         Text("W: \(Int(write), format: .number) GB/s")
+                            .font(.footnote)
+                            .fontDesign(.rounded)
+                            .fontWeight(.semibold)
+                            .fixedSize()
                     } icon: {
                         Image(systemName: "arrow.down")
                             .foregroundStyle(.primary.opacity(0.8))
-                            .fontWeight(.black)
+                            .fontWeight(.bold)
+                            .imageScale(.small)
                     }
                     .labelStyle(NoSpacingLabel(spacing: 3))
-//                    Image(systemName: "arrow.up")
-//                        .foregroundStyle(.primary.opacity(0.8))
-//                        
-//                    Image(systemName: "arrow.down")
-//                        .foregroundStyle(.primary.opacity(0.8))
-                        
-//                    Label {
-//                        Text("\(Int(total), format: .number) GB/s")
-//                    } icon: {
-//                        Image(systemName: "arrow.up.arrow.down")
-//                            .foregroundStyle(.primary.opacity(0.8))
-//                    }
-//                    .fontWeight(.bold)
-//                    VStack {
-//                        Text("R: \(Int(read), format: .number) GB/s")
-//                        Text("W: \(Int(write), format: .number) GB/s")
-//                    }
                     
                 }
-                .font(.footnote)
-                .fontDesign(.rounded)
-                .frame(alignment: .leading)
                 .scaleEffect(0.8)
+                .frame(width: 70)
+                
                 
             }
-        }
+        }.frame(maxWidth: .infinity, alignment: .leading)
         
         .onAppear {
             powerMonitor.start()
